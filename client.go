d22486b7f5b368a0548e56474205dc883faf02d0
rahman <rahman.hi@gmail.com>
2016-01-09 19:37:24 +0800

    Base structure & Handling Objects

---
+++ client.go
@@ -0,0 +1,66 @@
+package parse
+
+import (
+	"fmt"
+	"bytes"
+	"net/http"
+	"io/ioutil"
+	"encoding/json"
+)
+
+
+const HOST_NAME string = "https://api.parse.com"
+
+const API_VERSION string = "1"
+
+type Client struct {
+	AppId      string
+	RestKey    string
+	MasterKey  string
+}
+
+func (client Client) getRequest(url string) []byte {
+	return client.request("GET", url, nil)
+}
+
+func (client Client) postRequest(url string, params map[string]interface{}) []byte {
+	return client.request("POST", url, params)
+}
+
+func (client Client) putRequest(url string, params map[string]interface{}) []byte {
+	return client.request("PUT", url, params)
+}
+
+func (client Client) deleteRequest(url string) []byte {
+	return client.request("DELETE", url, nil)
+}
+
+func (client Client) request(method string, url string, params map[string]interface{}) []byte {
+	var response []byte
+	httpClient := &http.Client{}
+
+	jsonParams, _ := json.Marshal(params)
+	paramsReader := bytes.NewReader(jsonParams)
+
+	httpRequest, err := http.NewRequest(method, HOST_NAME + url, paramsReader)
+	httpRequest.Header.Add("X-Parse-Application-Id", client.AppId)
+	httpRequest.Header.Add("X-Parse-REST-API-Key", client.RestKey)
+	httpRequest.Header.Add("X-Parse-Master-Key", client.MasterKey)
+	httpResponse, err := httpClient.Do(httpRequest)
+
+	if err != nil {
+		fmt.Printf("Err1: %s", err)
+	} else {
+		defer httpResponse.Body.Close()
+
+		contents, err := ioutil.ReadAll(httpResponse.Body)
+
+		if err != nil {
+			fmt.Printf("Err2: %s", err)
+		} else {
+			response = contents
+		}
+	}
+
+	return response
+}
---
+++ object.go
@@ -0,0 +1,75 @@
+package parse
+
+import (
+	"fmt"
+	"encoding/json"
+)
+
+type ObjectType struct {
+	client *Client
+	className string
+}
+
+func (client Client) Object(className string) ObjectType {
+	return ObjectType{&client, className}
+}
+
+func (self ObjectType) GetAll() ([]map[string]interface{}) {
+	var url string = fmt.Sprintf("/%s/classes/%s/", API_VERSION, self.className)
+
+	var result map[string][]map[string]interface{}
+
+	response := self.client.getRequest(url)
+
+	json.Unmarshal(response, &result)
+
+	return result["results"]
+}
+
+func (self ObjectType) Get(objectId string) (map[string]interface{}) {
+	var url string = fmt.Sprintf("/%s/classes/%s/%s/", API_VERSION, self.className, objectId)
+
+	var result map[string]interface{}
+
+	response := self.client.getRequest(url)
+
+	json.Unmarshal(response, &result)
+
+	return result
+}
+
+func (self ObjectType) Create(object map[string]interface{}) (map[string]interface{}) {
+	var url string = fmt.Sprintf("/%s/classes/%s", API_VERSION, self.className)
+	
+	var result map[string]interface{}
+
+	response := self.client.postRequest(url, object)
+
+	json.Unmarshal(response, &result)
+
+	return result
+}
+
+func (self ObjectType) Update(object map[string]interface{}) (map[string]interface{}) {
+	var url string = fmt.Sprintf("/%s/classes/%s/%s", API_VERSION, self.className, object["objectId"])
+	
+	var result map[string]interface{}
+
+	response := self.client.putRequest(url, object)
+
+	json.Unmarshal(response, &result)
+
+	return result
+}
+
+func (self ObjectType) Delete(objectId string) (map[string]interface{}){
+	var url string = fmt.Sprintf("/%s/classes/%s/%s", API_VERSION, self.className, objectId)
+	
+	var result map[string]interface{}
+
+	response := self.client.deleteRequest(url)
+
+	json.Unmarshal(response, &result)
+
+	return result
+}
